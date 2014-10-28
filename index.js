13cca0f8109ed5390f1776db53b99d3d58fb3a1b
Jason Sanford <jasonsanford@gmail.com>
2014-10-27 20:24:39 -0400

    Proxy working.

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express'),
+    request = require('request'),
+    TileifyAGS = require('tileify-ags').TileifyAGS;
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, resp) {
+  resp.send('Hello World!');
+});
+
+app.get('/tiles/:z/:x/:y', function(req, resp) {
+  var z = parseInt(req.params.z, 10);
+  var x = parseInt(req.params.x, 10);
+  var y = parseInt(req.params.y, 10);
+
+  //pull out proxy query params
+  var agsServerUrl = req.query.url;
+  var redirect = req.query.redirect != null && req.query.redirect.toLowerCase() !== "false" ? true : false;
+
+  //grab all other query params intended for the ags server
+  var url_param_config = req.query;
+  delete url_param_config.url;
+  delete url_param_config.redirect;
+
+  //create the tile url
+  var tiler = new TileifyAGS(url_param_config);
+  var url = tiler.getTileUrl(agsServerUrl, x, y, z);
+
+  //serve it up!
+  if (redirect) {
+    resp.redirect(url);
+  } else {
+    req.pipe(request(url)).pipe(resp);
+  }
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Running at port: ' + app.get('port'));
+});
